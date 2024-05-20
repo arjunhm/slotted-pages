4a32dd1780ff13ca15be2f751048e7ebda29d1e1
arjunhm <hm.arjun97@gmail.com>
2024-05-19 23:59:14 -0400

    Added AddData func

--- page.go
+++ page.go
@@ -21,6 +21,10 @@ func putuint32(buf []byte, val uint32) {
 	binary.LittleEndian.PutUint32(buf, val)
 }
 
+func getuint32(buf []byte) uint32 {
+	return binary.LittleEndian.Uint32(buf)
+}
+
 func AllocPage(pageID uint32) *Page {
 	page := Page{
 		Data: make([]byte, PAGE_SIZE),
@@ -34,5 +38,33 @@ func AllocPage(pageID uint32) *Page {
 	return &page
 }
 
-//func (p *Page) AddData(key string, val string) {
-//}
+func (p *Page) AddData(key string, val string) {
+	// get free ptr offset
+	offset := getuint32(p.Data[4:8])
+
+	// get free slot array
+	count := getuint32(p.Data[8:12])
+
+	// get slot
+	slotOffset := HEADER_SIZE + (count * SLOT_SIZE)
+
+	// calculate length of data
+	keyLength := len(key)
+	valLength := len(val)
+	kvLength := keyLength + valLength
+	start := offset - kvLength
+
+	// add data to end of page
+	copy(p.Data[start: start + keyLength], key)
+	copy(p.Data[start + keyLength: offset], val)
+
+	// update free space pointer
+	putuint32(p.Data[4:8], uint32(start))
+
+	// add slot array
+	putuint32(p.Data[slotOffset: slotOffset+SLOT_POINTER_SIZE], uint32(start))
+	putuint32(p.Data[slotOffset+SLOT_POINTER_SIZE: slotOffset+SLOT_SIZE], uint32(kvLength))
+
+	// increment count
+	putuint32(p.Data[8:12], count+1)
+}
