473c34cac7c5009ae49f637a8751acdaa7e1da33
arjunhm <hm.arjun97@gmail.com>
2024-05-21 13:41:18 -0400

    Cleared out file.go

--- file.go
+++ file.go
@@ -4,41 +4,17 @@ import (
 	"os"
 )
 
-type DBFile struct {
-	File *os.File
+type File struct {
+	OSFile    *os.File
+	Directory Directory
+	Pages     []Page
 }
 
-func CreateFile(fileName string) (*DBFile, error) {
+func NewFile(fileName string) *File {
 
-	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		return nil, err
-	}
+	// create file in disk
+	// store file in File.OSFile
+	// create directory NewDirectory()
+	// create pages
 
-	return &DBFile{File: file}, nil
 }
-
-
-func (db *DBFile) ReadPage(pageID uint32) (*Page, error) {
-	offset := int(pageID - 1) * PAGE_SIZE
-	data := make([]byte, PAGE_SIZE)
-	_, err := db.File.ReadAt(data, offset)
-	if err != nil {
-		return nil, err
-	}
-	// look into this
-	return &Page{Data: data}, nil
-}
-
-func (db *DBFile) WritePage(page *Page, pageID uint32) error {
-	offset := int(pageID - 1) * PAGE_SIZE
-	_, err := db.File.WriteAt(page.Data, offset)
-	return err
-}
-
-func (db *DBFile) Close() error {
-	return db.File.Close()
-}
-
-
-
