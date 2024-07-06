25ef7f35816d70ea582a0ee9416b49fbaf7584ab
arjunhm <hm.arjun97@gmail.com>
2024-07-06 16:04:36 -0400

    Updated main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"page"
 )
 
 func main() {
-	fmt.Println("a")
+	fmt.Println("Slotted Pages Implementation")
+
+	//f := page.NewFile("temp.gob")
+	//fmt.Print(f)
+	s := page.NewSlot(1, 0, 0, 0)
+	fmt.Print(s)
 }
